7029672dc31409216d07841dcf2ba8eb8c64594d
breno <breno.al.ar@hotmail.com>
2023-03-19 14:03:36 -0300

    Update bird.py

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -3,7 +3,7 @@ import pygame
 from dino_runner.components.obstacles.obstacle import Obstacle 
 
 
-class Bird(Obstacle):
+'''class Bird(Obstacle):
     def __init__(self, images, velocidade = 2.0): #images[0,1]
         self.type =0
         super().__init__(images, self.type)
@@ -16,4 +16,20 @@ class Bird(Obstacle):
         if self.type == 2:
             self.type = 0    
         pygame.time.wait(int(100 / self.velocidade))
-  
+  '''
+
+class Bird(Obstacle):
+    def __init__(self, images): #images[0,1]
+        self.type =0
+        self.time = 0
+        super().__init__(images, self.type)
+        self.rect.y = random.randint(200, 260)
+
+    def draw(self, screen):
+        screen.blit(self.images[self.type], (self.rect.x, self.rect.y))
+        self.time += 1 
+        if self.time > 12: #Aqui controlo quanto tempo leva para Bird bate asas.
+            self.type += 1
+            self.time = 0
+        if self.type == 2:
+            self.type = 0    
