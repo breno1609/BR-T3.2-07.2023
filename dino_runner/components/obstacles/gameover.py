4edbc72b6d3672101578c77509fd6e52723a0148
breno <breno.al.ar@hotmail.com>
2023-03-20 09:47:35 -0300

    Update gameover.py

--- dino_runner/components/obstacles/gameover.py
+++ dino_runner/components/obstacles/gameover.py
@@ -2,12 +2,12 @@ import pygame
 
 class GameOver:
     def __init__(self):
-        self.x = 500
-        self.y = 300
-        self.font = pygame.font.SysFont('Arial Black', 40)
-        self.text = " "
+        self.x = 500 #POSIÇÃO DO X
+        self.y = 300 #POSIÇÃO DO Y
+        self.font = pygame.font.SysFont('Arial Black', 40) #DEFININDO A FONTE QUE SERA UTILIZADA E O TAMANHO
+        self.text = " " #CRIOANDO A VARIAVEL DE TEXTO
 
-    def draw(self, screen):
-        self.text = self.font.render('Game Over!', True, ((0, 0, 0)))
-        screen.blit(self.text, (self.x, self.y))
-        pygame.time.wait(1000)
+    def draw(self, screen): #FUNÇÃO PARA DESENHAR NA TELA
+        self.text = self.font.render('Game Over!', True, ((0, 0, 0))) #A COR DA LETTRA VAI SER PRETA, E O QUE É ESCRITO É ARMAZENADO NA VARIAVEL TEXT
+        screen.blit(self.text, (self.x, self.y)) #POSIÇÃO QUE A FRASE VAI FICAR NA TELA
+        pygame.time.wait(1000) #TEMPO DE ESPERA PARA APARECER A IMAGEM
