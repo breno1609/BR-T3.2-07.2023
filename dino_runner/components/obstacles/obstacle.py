e31d8c93cfa4b5a73de0e4218f09caf6652d8bc1
breno <breno.al.ar@hotmail.com>
2023-03-20 09:47:39 -0300

    Update obstacle.py

--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -2,15 +2,15 @@
 from dino_runner.utils.constants import SCREEN_WIDTH
  
 class Obstacle:
-    def __init__(self, images, type): #O modulo cactus.py traz para seu "pai" as imagens, num aleatorio, POS_Y
+    def __init__(self, images, type): #VAI RECEBER COMO PARAMETRO AS IMAGENS E O TIPO
         self.images = images
         self.type = type
         self.image = self.images[self.type]
         
         self.rect = self.image.get_rect()
-        self.rect.x = SCREEN_WIDTH
+        self.rect.x = SCREEN_WIDTH #VAI MOSTRAR NA TELA
 
-    def draw(self, screen):
+    def draw(self, screen): #DESENHA NA TELA
             screen.blit(self.image, (self.rect.x, self.rect.y))
   
     def update(self, game_speed, obstacles):
