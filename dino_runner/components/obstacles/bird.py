09b46f62beea3b3a30655a69ff1a927222841aa2
breno <breno.al.ar@hotmail.com>
2023-03-23 16:09:04 -0300

    Update bird.py

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -3,34 +3,34 @@ import pygame
 from dino_runner.components.obstacles.obstacle import Obstacle 
 
 
-class Bird(Obstacle): #CLASSE BIRD, HERDA A CLASSE OBSTACLE
-    def __init__(self, images): #CRIA O CONSTRUTOR E RECEBE AS IMAGENS COMO PARAMETRO
-        self.type =0 #VAI REPRESENTAR QUAL IMAGEM USAR DO PASSARINHO
-        self.time = 0 #VAI CONTROLAR A BATIDA DE ASAS DO BIRD
-        super().__init__(images, self.type) #CHAMA O CONSTRUTOR DA CLASSE PAI E PASSA OS ARGUMENTOS, VAI INICIAR O BIRD COM OS PARAMENTROS DA CLASSE PAI
-        self.rect.y = random.randint(200, 260) #VAI DEFINRI OS LOCAIS ALEATORIOS ONDE O PASSARO APARECE
+class Bird(Obstacle): 
+    def __init__(self, images): 
+        self.type =0 
+        self.time = 0 
+        super().__init__(images, self.type) 
+        self.rect.y = random.randint(200, 260) 
 
     def draw(self, screen):
-        screen.blit(self.images[self.type], (self.rect.x, self.rect.y)) #VAI DESENHAR NA TELA, O BIRD DE ACORDO COM A POSIÇÃO E A IMAGEM
+        screen.blit(self.images[self.type], (self.rect.x, self.rect.y)) 
         self.time += 1 
-        if self.time > 12: #ASE FOR MAIOR, MUDA A IMAGEM PARA O BIRD COM ASAS PRA BAIXO, ANIMA O BIRD
-            self.type += 1 #VAI MUDAR A IMAGEM DO PAASARO
-            self.time = 0 #VAI ZERAR O TEMPO, PRA VOLTAR A CONTAGEM
+        if self.time > 12: 
+            self.type += 1 
+            self.time = 0 
         if self.type == 2: 
             self.type = 0    
 
-class Bird(Obstacle): #CLASSE BIRD, HERDA A CLASSE OBSTACLE
-    def __init__(self, images): #CRIA O CONSTRUTOR E RECEBE AS IMAGENS COMO PARAMETRO
-        self.type =0 #VAI REPRESENTAR QUAL IMAGEM USAR DO PASSARINHO
-        self.time = 0 #VAI CONTROLAR A BATIDA DE ASAS DO BIRD
-        super().__init__(images, self.type) #CHAMA O CONSTRUTOR DA CLASSE PAI E PASSA OS ARGUMENTOS, VAI INICIAR O BIRD COM OS PARAMENTROS DA CLASSE PAI
-        self.rect.y = random.randint(200, 260) #VAI DEFINRI OS LOCAIS ALEATORIOS ONDE O PASSARO APARECE
+class Bird(Obstacle): 
+    def __init__(self, images): 
+        self.type =0 
+        self.time = 0 
+        super().__init__(images, self.type) 
+        self.rect.y = random.randint(200, 260) 
 
     def draw(self, screen):
-        screen.blit(self.images[self.type], (self.rect.x, self.rect.y)) #VAI DESENHAR NA TELA, O BIRD DE ACORDO COM A POSIÇÃO E A IMAGEM
+        screen.blit(self.images[self.type], (self.rect.x, self.rect.y))
         self.time += 1 
-        if self.time > 12: #ASE FOR MAIOR, MUDA A IMAGEM PARA O BIRD COM ASAS PRA BAIXO, ANIMA O BIRD
-            self.type += 1 #VAI MUDAR A IMAGEM DO PAASARO
-            self.time = 0 #VAI ZERAR O TEMPO, PRA VOLTAR A CONTAGEM
+        if self.time > 12: 
+            self.type += 1 
+            self.time = 0 
         if self.type == 2: 
             self.type = 0    
