38d32aa8eac4939919b28461772077026f907638
breno <breno.al.ar@hotmail.com>
2023-03-23 16:09:18 -0300

    Update moedas.py

--- dino_runner/components/obstacles/moedas.py
+++ dino_runner/components/obstacles/moedas.py
@@ -2,17 +2,17 @@ import random
 from dino_runner.components.obstacles.obstacle import Obstacle 
 
 class Moedas(Obstacle):
-    def __init__(self, images): #images[0,1]
-        self.type =0   #Primeira magem na lista
-        self.time = 0  #Intervalos para a animação
+    def __init__(self, images): 
+        self.type =0   
+        self.time = 0  
         super().__init__(images, self.type)
         self.rect.y = random.randint(200, 260)
 
     def draw(self, screen):
-        screen.blit(self.images[self.type], (self.rect.x, self.rect.y)) #VAI DESENHAR NA TELA, O BIRD DE ACORDO COM A POSIÇÃO E A IMAGEM
+        screen.blit(self.images[self.type], (self.rect.x, self.rect.y)) 
         self.time += 1 
-        if self.time > 12: #ASE FOR MAIOR, MUDA A IMAGEM PARA O BIRD COM ASAS PRA BAIXO, ANIMA O BIRD
-            self.type += 1 #VAI MUDAR A IMAGEM DO PAASARO
-            self.time = 0 #VAI ZERAR O TEMPO, PRA VOLTAR A CONTAGEM
+        if self.time > 12: 
+            self.type += 1 
+            self.time = 0 
         if self.type == 2: 
             self.type = 0 
