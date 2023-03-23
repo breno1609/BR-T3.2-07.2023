b71506b738c0a643dfad5576d669357824d266b8
breno <breno.al.ar@hotmail.com>
2023-03-23 16:09:10 -0300

    Update cactus.py

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -2,17 +2,17 @@ import random
 
 from dino_runner.components.obstacles.obstacle import Obstacle
 
-class Cactus(Obstacle): #HERDA DA CLASSE OBSTACULOS
-    def __init__(self, images): #MEOTOD CONSTRUTOR, QUE VAI RECEBER AS IMAGENS COMO PARAMETRO
-        self.type = random.randint(0,2) #VAI SORTEAR UM NUMERO, PARA SABER QUAL CACTO VAI APARECER
+class Cactus(Obstacle): 
+    def __init__(self, images):
+        self.type = random.randint(0,2) 
         super().__init__(images, self.type)
         
-        self.rect.y = 325 #POSIÇÃO ONDE O CACTU VAI APARECER
+        self.rect.y = 325 
 
 
-class LargeCactus(Obstacle): #HERDA DA CLASSE OBSTACULOS
-    def __init__(self, images): #VAI SORTEAR UM NUMERO, PARA SABER QUAL CACTO VAI APARECER
-        self.type = random.randint(0,2) #MEOTOD CONSTRUTOR, QUE VAI RECEBER AS IMAGENS COMO PARAMETRO
+class LargeCactus(Obstacle): 
+    def __init__(self, images):
+        self.type = random.randint(0,2) 
         super().__init__(images, self.type)
         
-        self.rect.y = 300  #POSIÇÃO ONDE O CACTU VAI APARECER 
+        self.rect.y = 300  
